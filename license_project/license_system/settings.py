f332f25890fc09970dc2713b77964a1fd07c9ca3
AnmolSingh28 <anmolsinghrathaud123@gmail.com>
2025-08-25 23:09:43 +0530

    Update settings.py and procfile

--- license_project/license_system/settings.py
+++ license_project/license_system/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["render.com",'https://django-license-tool.onrender.com']  
+ALLOWED_HOSTS = [ '127.0.0.1',
+    'localhost','https://django-license-tool.onrender.com']  
 
 
 # Application definition
