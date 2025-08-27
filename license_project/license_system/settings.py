f6fb60123818ae0c9efb6f4cab2cc10fef17ef3f
AnmolSingh28 <anmolsinghrathaud123@gmail.com>
2025-08-27 22:50:13 +0530

    Fixing problem in db

--- license_project/license_system/settings.py
+++ license_project/license_system/settings.py
@@ -92,11 +92,22 @@ WSGI_APPLICATION = 'license_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get("DATABASE_URL")
+if 'DATABASE_URL' not in os.environ:
+
+    DATABASES = {
+        'default':dj_database_url.config(
+            default=os.environ.get("DATABASE_URL"),
+            conn_max_age=600,
+            conn_health_checks=True,
     )
 }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE':'django.db.backends.sqlite3',
+            'NAME':BASE_DIR / 'db.sqlite3',
+        }
+    }
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Password validation
