0cd663e5d04645b941dd3f6d8451f22d98208de6
AnmolSingh28 <anmolsinghrathaud123@gmail.com>
2025-08-28 00:05:14 +0530

    wsgi error

--- license_project/license_system/wsgi.py
+++ license_project/license_system/wsgi.py
@@ -11,6 +11,6 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'license_system.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'license_project.license_system.settings')
 
 application = get_wsgi_application()
